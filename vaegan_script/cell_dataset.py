81b533deb8680ce71f89df91286da53bac242375
Akira Miyaki <42761934+a-miyaki@users.noreply.github.com>
2019-10-16 14:55:46 +0900

    Add files via upload

--- vaegan_script/cell_dataset.py
+++ vaegan_script/cell_dataset.py
@@ -1,42 +1,42 @@
-import torch
-import torch.utils.data
-import torchvision
-from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
 import os
 from PIL import Image
 import numpy as np
 
 
-def random_crop(img, crop_size=256):
-    _, h, w = img.shape
-    x_l = np.random.randint(0, w - crop_size)
-    x_r = x_l + crop_size
-    y_l = np.random.randint(0, h - crop_size)
-    y_r = y_l + crop_size
-    return img[:, y_l:y_r, x_l:x_r]
-
-
-class make_dataset(datasets):
-    def __init__(self, dataDir):
-
+class make_dataset(Dataset):
+    def __init__(self, dataDir, list, data_range):
         print("load dataset start")
         print("from : {}".format(dataDir))
 
+        self.dataset = []
         self.len = len(os.listdir(dataDir))
-        self.data_names = [os.path.join(dataDir, name) for name in sorted(os.listdir(dataDir))]
-
-        self.len = len(self.data_names)
+        for i in range(len(list)):
+            for j in range(data_range[0], data_range[1]):
+                full_path = dataDir + '/' + list[i] + '/c{0:04d}.png'.format(j)
+                img = Image.open(full_path)
+                label = list[i]
+                img = np.asarray(img)
+                img = np.asarray(img).astype("f").transpose(2, 0, 1) / 128.0 - 1.0
+                self.dataset.append((img, label))
 
     def __len__(self):
-        return self.len
+        return len(self.dataset)
 
     def __iter__(self):
         return self
 
-    def __getitem__(self, item):
-        data = Image.open(self.data_names[item]).convert('L')
-        label, _ = os.path.split(self.data_names[item])
-        data = random_crop(data)
-        data = data.transpose(2, 0, 1)
-        data = data.astype("float32") / 128.0 - 1.0
-        return (data.copy(), data.copy(), label)
+    def __getitem__(self, i, crop_size=256):
+        _, h, w = self.dataset[i][0].shape
+        x_l = np.random.randint(0, w - crop_size)
+        x_r = x_l + crop_size
+        y_l = np.random.randint(0, h - crop_size)
+        y_r = y_l + crop_size
+        return self.dataset[i][0][:, y_l:y_r, x_l:x_r], self.dataset[i][1]
+
+
+if __name__ == '__main__':
+    list = ["HEK-293", "KMST-6"]
+    dir = make_dataset('../testdata1', list, range(1141, 1900))
+    gen = DataLoader(dir, batch_size=128, shuffle=False, num_workers=1)
+
