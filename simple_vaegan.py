f0a9fff48e12c7b38e08b3ad1884a796afce8c6e
Akira Miyaki <42761934+a-miyaki@users.noreply.github.com>
2019-10-28 18:09:56 +0900

    Update simple_vaegan.py

--- simple_vaegan.py
+++ simple_vaegan.py
@@ -10,8 +10,12 @@ from datetime import datetime
 from torch.utils.data import Dataset, DataLoder
 
 
+print(" This program is prototype of simple VAEGAN ")
+print(" Start ")
+print("==================================================")
+
 parser = argsparse.ArgumentParser(description='Cell Feature of VaeGan')
-parser.add_argument('--batch_size', type=int, default=4)
+parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--epochs' type=int, default=100)
 parser.add_argument('--no_cuda', action='store_true', default=False)
 parser.add_argument('--dataset', default='./traindata')
@@ -23,16 +27,18 @@ args.parser.parse_args()
 class make_dataset(Dataset):
 	def __init__(self, dir, list, range):
 		print(" Load start ")
+		print("from : {}, range : {} ",format(dir, range))
 		self.dataset = []
-        self.len = len(os.listdir(dataDir))
+        self.len = len(os.listdir(dir))
         for i in range(len(list)):
             for j in range(data_range[0], data_range[1]):
-                full_path = dataDir + '/' + list[i] + '/c{0:04d}.png'.format(j)
+                full_path = dir + '/' + list[i] + '/c{0:04d}.png'.format(j)
                 img = Image.open(full_path)
                 label = list[i]
                 img = np.asarray(img)
                 img = np.asarray(img).astype("f").transpose(2, 0, 1) / 128.0 - 1.0
                 self.dataset.append((img, label))
+		print(" Load done")
 
     def __len__(self):
         return len(self.dataset)
@@ -71,17 +77,8 @@ class VAE(nn.Module):
 		decoder_list.append(nn.ConvTranspose2d(in_channels=64, out_channels=in_ch, kernel_size=kn, padding=p, stride=s)
 		self.deconv = nn.Sequential(*decoder_list)
 		
-		disc_list = []
-		self.fc4 = (nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channles=32, kernel_size=kn, padding=p, stride=s),
-										nn.ReLU(Ture))
-		disc_list.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, padding=p, stride=2)
-		disc_list.append(nn.Conv2d(in_channles=128, out_channels=256, kernel_size=4, padding=p, stride=2)
-		self.disconv = nn.Sequential(*disc_list)
-		self.fc5 = nn.Sequential(nn.Linear(in_features=64*64*256, out_features=512),
-								nn.ReLU(True),
-								nn.Linear(in_features=512, out_features=1))
 		
-	def forward(self, ten, disc_ten):
+	def forward(self, ten):
 		ten_original = ten
 		ten = self.conv(ten)
 		ten = self.fc1(ten)
@@ -94,18 +91,33 @@ class VAE(nn.Module):
 		ten = self.fc2(mu)
 		ten = self.fc3(ten)
 		ten = self.deconv(ten)
-		ten_dis = torch.cat(self.fc4(ten), self.fc4(ten_original), 0)
-		ten_dis = self.disconv(ten_dis)
-		ten_dis = self.fc5(ten_dis)
 		return ten, mu, logvar, ten_dis
 
 
 class GAN(nn.Module):
-	
+	def __init__(self, in_ch=3, out_ch=3, z_size=256, kn=3, p=1, s=1):
+		super(GAN, self).__init__()
+		disc_list = []
+		self.fc4 = (nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channles=32, kernel_size=kn, padding=p, stride=s),
+										nn.ReLU(Ture))
+		disc_list.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, padding=p, stride=2)
+		disc_list.append(nn.Conv2d(in_channles=128, out_channels=256, kernel_size=4, padding=p, stride=2)
+		self.disconv = nn.Sequential(*disc_list)
+		self.fc5 = nn.Sequential(nn.Linear(in_features=64*64*256, out_features=512),
+								nn.ReLU(True),
+								nn.Linear(in_features=512, out_features=1))
+
+	def forward(self, ten, ten_original):
+		ten_dis = torch.cat(self.fc4(ten), self.fc4(ten_original))
+		ten_dis = self.disconv(ten_dis)
+		ten_dis = self.fc5(ten_dis)
+		return ten_dis
 
 
-model = VAEGAN()
+model = VAE()
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
+dis = GAN()
+dis_optimizer = optim.Adam(dis.parameters(), lr=1e-3)
 
 cell_list = os.listdir(args.dataset)
 train_loader = make_dataset(args.dataset, cell_list, range=(1, 100)
@@ -118,15 +130,61 @@ def loss_function(ten, ten_original, mu, logvar):
 	return BCE + KLD
 
 
-def dis_loss_function(fake, real, target,
+def dis_loss_function(fake, real, target, data_size=(256, 256))
+	y_in = dis(real, target)
+	y_out = dis(fake, target)
+	L1 = torch.sum(nn.softmax(-y_in)) / args.batch_size / data_size[0] / data_size[1]
+	L2 = torch.sum(nn.softmax(y_out)) / args.batch_size / data_size[0] / data_size[1]
+	return L1 + L2
 
 
 def train(epoch):
 	model.train()
-	train_loss = 0
+	train_loss, train_dis_loss = 0, 0
 	for batcg_idx, (data, _) in enumerate(train_loader):
 		data = data.to(device)
 		optimizer.zero_grad()
-		ten, mu, logvar, ten_dis = model(data)
-		loss = loss_function(ten, data, mu, logvar)
+		dis_optimizer.zero_grad()
+		ten, mu, logvar = model(data)
+		train_loss += loss_function(ten, data, mu, logvar)
+		train_dis_loss += dis_loss_function(ten, data, data)
+		loss.backward()
+		train_dis_loss.backward()
+		optimizer.step()
+		dis_optimier.step()
+	train_loss /= len(train_loader.dataset).item()
+	train_dis_loss /= len(train_loader.dataset).item()
+	return train_loss, train_dis_loss
+
+
+def test(epoch):
+	model.eval()
+	test_loss, test_dis_loss = 0, 0
+	for batch_idx, (data, _) in enumerate(test_loader):
+		data = data.to(device)
+		ten, mu, logvar = model(data)
+		test_loss += loss_function(ten, data, mu, logvar).item()
+		test_dis_loss += dis_loss_function(ten, data, data).item()
+		if epoch % 10 == 0 and batch_idx == 0:
+			n = 1
+			comparsion = torch.cat([data[:n], ten.view(args.batch_size, 1, 300, 300)[:n]])
+			save_image(comparsion.data.cpu(), '{}/reconstruction_{}.png'.format(args.out, epoch), nrow=n)
+	test_loss /= len(test_loader.dataset)
+	test_dis_loss /= len(test_loader.dataset)
+	return test_loss, test_dis_loss
+
+
+def main():
+	print("device : {}".format(device))
+	train_loss_list = []
+	train_dis_loss_list = []
+	for epoch in range(1, args.epochs + 1):
+		t1 = time.time()
+		tr_loss, tr_dis_loss = train(epoch)
+		te_loss, te_dis_loss = test(epoch)
+		t2 = time.time()
+		print('epoch [{} / {}], train loss : {:.4f}, trian discriminator loss : {:.4f}'.format(epoch, args.epochs, tr_loss, tr_dis_loss))
+		print('                 test loss  : {:.4f}, test discriminator loss  : {:.4f}'.format(te_loss, te_dis_loss))
+		print('time / 1 epoch : {:.4f} sec, remaining time : {:.4f} min'.format(t2 - t1, (t2 - t1) * (args.epochs - epoch + 1) / 60))
+		train_loss_list.append(tr_loss, tr_dis_loss)
 		
